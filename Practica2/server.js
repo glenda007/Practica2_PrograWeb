4507caa9f0327bfc71d16e7a01b72f7fadd0a125
glenda007 <chocglenda@umes.edu.gt>
2025-09-09 23:37:48 -0600

    cambios

--- Practica2/server.js
+++ Practica2/server.js
@@ -1,28 +1,26 @@
 import express from 'express';
 import routes from './routes/index.route.js';
 import dotenv from 'dotenv';
-import { connectDB } from './configs/mongoose.config.js';
+import { connectToMongo } from './configs/mongoose.config.js';
+import i18nMiddleware from './middlewares/i18n.middleware.js';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(i18nMiddleware);
 
-app.use("/",routes);
+app.use("/", routes);
 
 const startServer = async () => {
   try {
-    await connectDB();
-    app.listen(port, () => {
-      console.log(`Servidor escuchando en el puerto ${port}`);
-    });
+    await connectToMongo();
+    app.listen(port, () => console.log('Servidor iniciado'));
   } catch (error) {
-    console.error('Error al iniciar el servidor:', error);
-    throw error;
+    console.error("Error al iniciar el servidor:", error);  
   }
-};
+}
 
 startServer();
